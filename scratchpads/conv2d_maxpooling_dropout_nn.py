6c43f273ad0e9f1f9418af8cb0d4603bf4ac0cc4
Matthew Zimmer <M16r01@gmail.com>
2016-12-07 00:05:57 -0500

    finished training cnn with keras framework. Go 0.9921 validation accuracy.

--- scratchpads/conv2d_maxpooling_dropout_nn.py
+++ scratchpads/conv2d_maxpooling_dropout_nn.py
@@ -1,6 +1,9 @@
-from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D
+# TODO: Re-construct the network and add dropout after the pooling layer.
+# TODO: Compile and train the model.
+
+from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, Dropout
 from keras.models import Sequential
-from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
 
 from zimpy.datasets.german_traffic_signs import GermanTrafficSignDataset
 
@@ -11,14 +14,18 @@ X_train = data.train_orig
 y_train = data.train_labels
 
 X_train = data.normalize_data(X_train)
+X_train = X_train.astype('float32')
+
+# convert class vectors to binary class matrices
+# Y_train = np_utils.to_categorical(y_train, nb_classes)
 
 # TODO: Re-construct the network and add a convolutional layer before the first fully-connected layer.
 model = Sequential(name='input')
-model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(32, 32, 3)))
-# model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(32, 32, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dense(128, input_dim=32*32*3, name='hidden1'))
+model.add(Dense(128, name='hidden1'))
 model.add(Activation('relu'))
 model.add(Dense(data.num_classes))
 model.add(Activation('softmax', name='output'))
@@ -28,14 +35,11 @@ model.summary()
 # TODO: Compile and train the model.
 batch_size = 128
 nb_classes = data.num_classes
-nb_epoch = 1
-
+nb_epoch = 2
 
 model.compile(optimizer='adam',
           loss='categorical_crossentropy',
           metrics=['accuracy'])
-
-from sklearn.model_selection import train_test_split
 
 # TODO: Split some of the training data into a validation dataset.
 X_train, X_val, y_train, y_val = train_test_split(
@@ -50,6 +54,7 @@ history = model.fit(X_train, y_train,
                     verbose=1, validation_data=(X_val, y_val))
 
 # score = model.evaluate(X_val, y_val, verbose=1)
+# print('Validation (loss, accuracy): (%.3f, %.3f)' % (score[0], score[1]))
 
 # STOP: Do not change the tests below. Your implementation should pass these tests.
 assert(history.history['val_acc'][0] > 0.9), "The validation accuracy is: %.3f" % history.history['val_acc'][0]
---
+++ scratchpads/german_traffic_sign_cnn.py
@@ -0,0 +1,87 @@
+from keras.layers import Dense, Activation, Flatten, Convolution2D, MaxPooling2D, Dropout
+from keras.models import Sequential
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+from keras import backend as K
+
+from zimpy.datasets.german_traffic_signs import GermanTrafficSignDataset
+
+data = GermanTrafficSignDataset()
+data.configure(one_hot=True, train_validate_split_percentage=0)
+
+X_train = data.train_orig
+y_train = data.train_labels
+
+X_train = data.normalize_data(X_train)
+X_train = X_train.astype('float32')
+
+batch_size = 128
+nb_classes = data.num_classes
+nb_epoch = 12
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+
+# number of convolutional filters to use
+nb_filters = 32
+
+# number of channels for our input image
+nb_channels = 3
+
+# convolution kernel size
+kernel_size = (5, 5)
+
+# size of pooling area for max pooling
+pool_size = (2, 2)
+
+# number of neurons for our hidden layer
+hidden_layer_neurons = 128
+
+# A float between 0 and 1. Fraction of the input units to drop.
+dropout_p_1, dropout_p_2 = 0.5, 0.5
+
+# If Theano backend, input_shape is different so let's take care of that first
+if K.image_dim_ordering() == 'th':
+    X_train = X_train.reshape(X_train.shape[0], nb_channels, img_rows, img_cols)
+    input_shape = (nb_channels, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, nb_channels)
+    input_shape = (img_rows, img_cols, nb_channels)
+
+
+# build the model
+model = Sequential(name='input')
+model.add(Convolution2D(16, kernel_size[0], kernel_size[1], border_mode='valid', input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+model.add(Dropout(dropout_p_1))
+model.add(Flatten())
+model.add(Dense(hidden_layer_neurons, name='hidden1'))
+model.add(Activation('relu'))
+# model.add(Dropout(dropout_p_2))
+model.add(Dense(data.num_classes))
+model.add(Activation('softmax', name='output'))
+
+# print information about the model itself
+model.summary()
+
+# Compile and train the model.
+model.compile(optimizer='adam',
+          loss='categorical_crossentropy',
+          metrics=['accuracy'])
+
+# Split some of the training data into a validation dataset.
+X_train, X_val, y_train, y_val = train_test_split(
+            X_train,
+            y_train,
+            test_size=0.25,
+            random_state=832224)
+
+history = model.fit(X_train, y_train,
+                    batch_size=batch_size, nb_epoch=nb_epoch,
+                    verbose=1, validation_data=(X_val, y_val))
+
+# STOP: Do not change the tests below. Your implementation should pass these tests.
+assert(history.history['val_acc'][-1] > 0.9), "The validation accuracy is: %.3f" % history.history['val_acc'][-1]
